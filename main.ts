3fb2d55c342a76765898e56f769102949334ab9b
Jdmg1308 <70779353+Jdmg1308@users.noreply.github.com>
2020-09-04 16:38:12 -0500

    Personaje y movimiento básico

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . f f . . . . 
+    . . . . . . . . . f . f . . . . 
+    . . . . . . . . f . . f . f . . 
+    . . . . . . . f . . . f . f . . 
+    . . . . . f f . . f f . f . . . 
+    . . f f f f f f f f f f . . . . 
+    . f f . . f . . f . f . . . . . 
+    f . . . . . . . . . f . . . . . 
+    . . . . . . . . . f . . . . . . 
+    . . . . . . . . . f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+scene.setBackgroundColor(1)
+controller.moveSprite(mySprite)
